a450a9e5a596bdd3fdf54bfaebf02df900bc512e
Sophia <sophiaduan1997@gmail.com>
2017-01-20 22:20:10 -0500

    there is some issue

--- face.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-import glob
-image_list = []
-for filename in glob.glob('/Users/duanyuqin/Documents/knockknock/group.JPG'): 
-	im = Image.open(filename)
-	image_list.append(im)
