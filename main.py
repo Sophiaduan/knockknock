a134eacc241ca3038d775b85d896512d372065d7
Mehmet Alp Aysan <maysan@pal-nat186-113-193.itap.purdue.edu>
2017-01-20 20:35:10 -0600

    trial 1

---
+++ main.py
@@ -0,0 +1,13 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 12:39:47) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> 
+====== RESTART: /Users/maysan/Desktop/Education/Boiler Make IV/main.py ======
+Hello World
+>>> 
+====== RESTART: /Users/maysan/Desktop/Education/Boiler Make IV/main.py ======
+Hello World
+>>> 
