a009df5d46bfddadee75fd6623aeb5be27b525a1
Mehmet Alp Aysan <maysan@pal-nat186-120-177.itap.purdue.edu>
2017-01-21 11:26:45 -0500

    Commit with Working Save Function

--- saveface.py
+++ saveface.py
@@ -2,17 +2,12 @@ import pandas as pd
 import numpy as np 
 import math
 import csv
-node2 = dict()
 
-data = ["I am the best"]
-indentify = False
-##if node2 == None:
-##	new_node = pd.DataFrame.from_dict(node2, orient = 'index')
-##	new_node.to_csv('facesave.csv')
-
-with open (r'facesave.csv', 'a') as f:
-	if indentify == False:
-		writer = csv.writer(f)
-		writer.writerow(data+["Sophia"])
-		#writer.writerow(["Sophia"])
+def saveface(name, id):
+        node2 = dict()
+        data = [str(name)]
+        
+        with open (r'facesave.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(data+[str(id)])
 
